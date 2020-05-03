836a115977785ca4c7f50e2db9e0af1d7d6f1041
Nada Boukhari <nadaboukhari2015@gmail.com>
2020-05-02 22:24:48 -0700

    finished First problem of Homework 2

--- EvenOdd/src/evenodd/EvenOdd.java
+++ EvenOdd/src/evenodd/EvenOdd.java
@@ -5,9 +5,13 @@
  */
 package evenodd;
 
+import static java.lang.Math.pow;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  *
- * @author Curveball
+ * @author Nada Boukhari
  */
 public class EvenOdd {
 
@@ -15,7 +19,53 @@ public class EvenOdd {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+
+        int[] s = {1,2,2};
+        //System.out.println(s[3]);
+        int[] temp = new int[s.length];
+        int index = 0;
+        int cnt = 0;
+        int m=0;
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        solve(s, index, temp, cnt,list);
+        
+        int evenSequences = (int) ((pow(2,s.length) -1) - list.size());
+        System.out.println("\nNumber of sequences that sum to an odd number: "+list.size());
+        System.out.println("Number of sequences that sum to an even number: "+evenSequences);
     
+
+
+    }
+
+    private static void solve(int[] arrInput, int index, int[] temp, int cnt, LinkedList<Integer> l) {
+        if (index == arrInput.length) {
+            String result = "";
+            int r = 0;
+            for (int i = 0; i < temp.length; i++) {
+                if (temp[i] == 1) {
+                    result += arrInput[i] + " ";
+                    r += arrInput[i];
+                    /*if (r % 2 != 0) {
+                        cnt++;
+                    }*/
+                }
+            }
+            if(result ==""){
+                return;
+            }
+            if (r % 2 != 0) {
+                l.add(1);
+                System.out.println(result);
+            } else {
+                System.out.println(result + " sum = " + r + " even");
+            }
+            return;
+        }
+        //set the current index bit and solve it recursively
+        temp[index] = 1;
+        solve(arrInput, index + 1, temp, cnt,l);
+        //unset the current index bit and solve it recursively
+        temp[index] = 0;
+        solve(arrInput, index + 1, temp, cnt,l);
+    }
 }

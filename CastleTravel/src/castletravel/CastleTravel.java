0ef02e8da4f1fe7ee37c6b75749cab83919262ee
Nada Boukhari <nadaboukhari2015@gmail.com>
2020-05-17 22:14:52 -0700

    completed assignment

--- CastleTravel/src/castletravel/CastleTravel.java
+++ CastleTravel/src/castletravel/CastleTravel.java
@@ -5,8 +5,12 @@
  */
 package castletravel;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 /**
  *
@@ -14,17 +18,58 @@ import java.util.LinkedList;
  */
 public class CastleTravel {
 
-    public static void main(String[] args) {
-        int[] castles = {0, 6, 9, 10, 11, 17, 18, 23, 25, 31, 36, 40, 44, 50};
-        int[] others = {0, 5, 10, 15, 20, 25, 30, 35};
-        int[] test = {0, 1, 2, 3, 4, 5, 10, 12, 15};
-        int travel = 8;
-        int[][] lookup = new int[travel + 1][test.length + 1];
+    static int INF = Integer.MAX_VALUE;
 
-        int start;
-        int end;
+    public static void main(String[] args) throws FileNotFoundException {
+        //int[] castles = {0, 6, 9, 10, 11, 17, 18, 23, 25, 31, 36, 40, 44, 50};
+        //int[] others = {0, 5, 10, 15, 20, 25, 30, 35};
+        //int[] test = {0, 1, 2, 3, 4, 5, 10, 12, 15};
+        //int travel = 4;
+        //int[][] lookup = new int[travel + 1][test.length + 1];
+        String[] arr;
+
+        File input = null;
+        if (args.length == 1) {
+            input = new File(args[0]);
+        } else {
+            System.err.println("Invalid input");
+            System.exit(0);
+        }
+
+        int pos = 0;
+        int temp = INF;
+
+        Scanner sc = new Scanner(input);
+        int travel = Integer.parseInt(sc.nextLine());
+
+        arr = sc.nextLine().split("\t");
+        int[] castles = new int[arr.length];
+        for (int i = 0; i < castles.length; i++) {
+            castles[i] = Integer.parseInt(arr[i]);
+        }
+
+        int[] minPenalty = new int[castles.length];
+        minPenalty[0] = 0;
+
+        for (int i = 1; i < minPenalty.length; i++) {
+            for (int k = 0; k < i; k++) {
+                if (temp > minPenalty[k] + (int) Math.pow((travel - (castles[i] - castles[k])), 2)) {
+                    temp = minPenalty[k] + (int) Math.pow((travel - (castles[i] - castles[k])), 2);
+                    pos = k;
+                }
+            }
+            minPenalty[i] = temp;
+            System.out.println("got to " + castles[i] + " from " + castles[pos]);
+            temp = INF;
+        }
+        System.out.println("Castles :" + Arrays.toString(castles));
+        System.out.println("Penalties :" + Arrays.toString(minPenalty));
+        //System.out.println("Minimized :" +Arrays.toString(minimizedBy));
+
+        /*int end;
         int[] admitted = new int[travel];
-
+        
+        int start =0;
         LinkedList<Integer> list = new LinkedList<>();
 
         for (int i = 0; i < lookup.length; i++) {
@@ -39,9 +84,6 @@ public class CastleTravel {
             lookup[0][i + 1] = test[i];
         }
 
-        //System.out.println(Arrays.toString(admitted));
-
-        
         int cnt;
         start = lookup[0][1];
         int k = 0;
@@ -74,13 +116,13 @@ public class CastleTravel {
                 }
             }
 
-            
             list.clear();
         }
-        
+
         for (int i = 0; i < lookup.length; i++) {
             System.out.println(Arrays.toString(lookup[i]));
         }
+
 
         /*for (int i = 0; i < castles.length; i++) {
             k = i;

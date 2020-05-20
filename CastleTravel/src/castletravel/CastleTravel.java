b08f960f12f9ae8f62e67a1b66bcfd6ba5af8cbc
Nada Boukhari <nadaboukhari2015@gmail.com>
2020-05-19 18:31:47 -0700

    final version

--- CastleTravel/src/castletravel/CastleTravel.java
+++ CastleTravel/src/castletravel/CastleTravel.java
@@ -1,29 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package castletravel;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Deque;
-import java.util.LinkedList;
 import java.util.Scanner;
 
 /**
  *
- * @author Curveball
+ * @author Nada Boukhari
  */
 public class CastleTravel {
 
     static int INF = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws FileNotFoundException {
-        
+
+        // Get the file
         File input = null;
         if (args.length == 1) {
             input = new File(args[0]);
@@ -32,56 +25,78 @@ public class CastleTravel {
             System.exit(0);
         }
 
-        String[] arr;
+        //convert file data to miles and castles (array of castle positions)
+        String[] str;
         Scanner sc = new Scanner(input);
-        int t = Integer.parseInt(sc.nextLine());
-        arr = sc.nextLine().split("\t");
-        int[] c = new int[arr.length];
+        int m = Integer.parseInt(sc.nextLine());
+        str = sc.nextLine().split("\t");
+        int[] c = new int[str.length];
         for (int i = 0; i < c.length; i++) {
-            c[i] = Integer.parseInt(arr[i]);
+            c[i] = Integer.parseInt(str[i]);
         }
-        
-        int whereTo = c.length -1;
-        
-        Traveling(c,t,whereTo);
+        //get index of the last castle position, i.e the final destination
+        int end = c.length - 1;
+
+        //call Journey method
+        Journey(c, m, end);
 
     }
 
-    public static void Traveling(int[] castles, int travel,int whereTo) {
+    public static void Journey(int[] castles, int miles, int end) {
+        //variable declarations
         int pos = 0;
-        int temp = INF;
-        int[] minPenalty = new int[castles.length];
+        int minPenalty = INF;
+        int stopIndex;
+        int[] Penalty = new int[castles.length];
         int[] minimizedBy = new int[castles.length];
-        minPenalty[0] = 0;
-
-        for (int i = 1; i < minPenalty.length; i++) {
-            for (int k = 0; k < i; k++) {
-                if (temp > minPenalty[k] + (int) Math.pow((travel - (castles[i] - castles[k])), 2)) {
-                    temp = minPenalty[k] + (int) Math.pow((travel - (castles[i] - castles[k])), 2);
-                    pos = k;
+        Penalty[0] = 0;
+        
+        /////////Main Algorithm/////////////
+        
+        //loop through the all the castle positions
+        for (int i = 1; i < Penalty.length; i++) {
+            //for every new castle position, check the penalty between
+            //the castle at position 0,1,2... and the current i position until i is reached
+            for (int j = 0; j < i; j++) {
+                //if the new position has a smaller penalty than the previous one, update the penalty (get the minimim penalty)
+                if (minPenalty > Penalty[j] + (int) Math.pow(((castles[i] - castles[j]) - miles), 2)) {
+                    minPenalty = Penalty[j] + (int) Math.pow(((castles[i] - castles[j]) - miles), 2);
+                    //store the position of the newest castle to yield a small penalty
+                    pos = j;
                 }
             }
-            minimizedBy[i]=pos;
-            minPenalty[i] = temp;
-            temp = INF;
+            //store the positions that yield the smallest penalty for each castle
+            minimizedBy[i] = pos;
+            //store the minimum penalty for each castle
+            Penalty[i] = minPenalty;
+            //update the minimum penalty to loop again
+            minPenalty = INF;
         }
-        //System.out.println("Minimized :"+ Arrays.toString(minimizedBy));
-        //System.out.println("Castles :" + Arrays.toString(castles));
-        //System.out.println("Penalties :" + Arrays.toString(minPenalty));
-        int kk;
         
-        Deque<Integer> stack = new ArrayDeque<Integer>();
-        stack.push(castles[castles.length-1]);
-        while(minPenalty[whereTo]>0){
-            kk= minimizedBy[whereTo];
-            stack.push(castles[kk]);    
-            whereTo = kk;
-        }       
-        System.out.print("You should stop at:\t");
-        while(!stack.isEmpty()){
-            System.out.print(stack.pop()+"\t");
+        //create a stack and push the last castle
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(castles[end]);
+        
+        //As long as the castle at position 0 isn't reached, 
+        //start from the last castle and travel back to the
+        //first depending on which stops yield the smallest penalty
+        while (end > 0) {
+            stopIndex = minimizedBy[end];
+            //push castles in which to stop in stack
+            stack.push(castles[stopIndex]);
+            end = stopIndex;
         }
-        System.out.println("\nThe penalty is:\t"+minPenalty[castles.length-1]);
+
+        System.out.print("Stop at:\t");
+        
+        //as long as the stack still has castles, pop them
+        //to reveal the path that yields the smallest penalty
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + "\t");
+        }
+        
+        //retrieve the penalty for the overall journey
+        System.out.println("\nThe penalty is:\t" + Penalty[castles.length - 1]);
     }
 
 }

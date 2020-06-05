bc76a3a508bc3857f883997041d66c2b46cd774b
Nada Boukhari <nadaboukhari2015@gmail.com>
2020-06-05 09:18:37 -0700

    working code

--- TrufflePath/src/trufflepath/TrufflePath.java
+++ TrufflePath/src/trufflepath/TrufflePath.java
@@ -5,8 +5,16 @@
  */
 package trufflepath;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
 import static java.lang.Math.max;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
 
 /**
  *
@@ -14,129 +22,161 @@ import static java.lang.Math.max;
  */
 public class TrufflePath {
 
+    static int INF = Integer.MIN_VALUE;
+
+    public static class TruffleInfo {
+
+        public int row;
+        public int column;
+        public int truffles;
+
+    }
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        /*int[][] c = new int[4][6];
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        /*int[][] c = new int[7][20];
         for (int i = 0; i < c.length; i++) {
             for (int j = 0; j < c[i].length; j++) {
-                c[i][j] = 0;
+                c[0][j] = 100;
             }
-            c[i][i]=1;
+            //c[i][0] = 0;
         }
-        c[c.length-1][0] = 100;*/
-        //this configuration is fucked up for reasons i do not understand !!!!!!
-        
-        
-        int[][] f
-                = {
-                    {9, 5, 10, 5, 1, 2},
-                    {10, 3, 10, 3, 3, 4},
-                    {2, 3, 4, 4, 4, 2},
-                    {2, 2, 3, 2, 2, 3},
-                    {2, 2, 4, 3, 4, 2},
-                    {4, 4, 4, 4, 2, 3}};
+        c[c.length - 1][0] = 10;
+        c[2][1] = 400;
+        c[3][1] = 400;
+        c[4][1] = 400;
+        c[3][8] = 50000;
+        c[c.length - 1][10] = 1000;*/
 
-        int truffleYield;
+ /*File input = null;
+        if (args.length == 1) {
+            input = new File(args[0]);
+        } else {
+            System.err.println("Invalid input");
+            System.exit(0);
+        }*/
+        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\boukh\\Desktop\\field.txt"));
+        br.mark(0);
+        int rows = 0;
+        int columns = 0;
+        String[] n;
+        while (br.ready()) {
+            String line = br.readLine();
+            n = line.split("\t");
+            rows++;
+            columns = n.length;
+        }
 
-        truffleYield = TruffleHarvest(f);
+        BufferedReader br2 = new BufferedReader(new FileReader("C:\\Users\\boukh\\Desktop\\field.txt"));
+        int[][] field = new int[rows][columns];
+        int k = 0;
+        while (br2.ready()) {
+            String line = br2.readLine();
+            String[] tokens = line.split("\t");
+            // now put each in 2D array
+            for (int j = 0; j < tokens.length; j++) {
+                field[k][j] = Integer.parseInt(tokens[j]);
+            }
+            k++;
+        }
 
-        System.out.println(truffleYield);
+        /*for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                System.out.print("\t" + field[i][j]);
+            }
+            System.out.print("\n");
+        }*/
+        int truffles = TruffleHarvest(field);
+        System.out.println(truffles + " truffles");
     }
 
     static int TruffleHarvest(int field[][]) {
-        // To find max val in first row 
-        int[][] y = new int[field.length][field[0].length];
+        int[][] currentYield = new int[field.length][field[0].length];
         int[][] path = new int[field.length][field[0].length];
 
         for (int i = 0; i < field.length; i++) {
             for (int j = 0; j < field[i].length; j++) {
-                y[i][j] = field[i][j];
+                currentYield[i][j] = field[i][j];
             }
         }
-        int[] temp = new int[field.length];
-        int truffle = -1;
-        for (int i = 0; i < y[0].length; i++) {
-            if (y[0][i] >= truffle) {
-                truffle = y[0][i];
-                temp[0] = i;
+        //int[] temp = new int[field.length];
+        int maxTruffles = INF;
+        int lastRowMaxIndex= 0;
 
-            }
-            path[0][i] = i;
-        }
-
-        for (int i = 1; i < y.length; i++) {
-            truffle = -1;
-            for (int j = 0; j < y[0].length; j++) {
+        for (int i = 1; i < currentYield.length; i++) {
+            maxTruffles = INF;
+            for (int j = 0; j < currentYield[0].length; j++) {
                 // When all paths are possible 
-                if (j > 0 && j < y[0].length - 1) {
-                    if (y[i - 1][j - 1] > y[i - 1][j + 1]) {
-                        path[i][j] = j - 1;
-                    } else {
-                        if (y[i - 1][j] > y[i - 1][j + 1]) {
-                            path[i][j] = j;
-                        } else {
-                            path[i][j] = j + 1;
-                        }
-                    }
-                    y[i][j] = max(y[i - 1][j], max(y[i - 1][j - 1], y[i - 1][j + 1])) + y[i][j];
-                    //path[i][j] = j;
-                } // When diagonal right is not possible 
-                else if (j > 0) {
-                    if (y[i - 1][j] > y[i - 1][j - 1]) {
+                if (j > 0 && j < currentYield[0].length - 1) {
+                    currentYield[i][j] = max(currentYield[i - 1][j], max(currentYield[i - 1][j - 1], currentYield[i - 1][j + 1])) + currentYield[i][j];
+                    if (currentYield[i][j] == currentYield[i - 1][j] + field[i][j]) {
                         path[i][j] = j;
-                    } else {
+                    } else if (currentYield[i][j] == currentYield[i - 1][j - 1] + field[i][j]) {
                         path[i][j] = j - 1;
-                    }
-                    y[i][j] = max(y[i - 1][j], y[i - 1][j - 1]) + y[i][j];
-                    //path[i][j] = j;
-                } // When diagonal left is not possible 
-                else if (j < y[0].length - 1) {
-                    if (y[i - 1][j] > y[i - 1][j + 1]) {
-                        path[i][j] = j;
-                    } else {
+                    } else if (currentYield[i][j] == currentYield[i - 1][j + 1] + field[i][j]) {
                         path[i][j] = j + 1;
                     }
-                    y[i][j] = max(y[i - 1][j], y[i - 1][j + 1]) + y[i][j];
-                    //path[i][j] = j;
+                } // When diagonal right is not possible 
+                else if (j > 0) {
+                    currentYield[i][j] = max(currentYield[i - 1][j], currentYield[i - 1][j - 1]) + currentYield[i][j];
+                    if (currentYield[i][j] == currentYield[i - 1][j] + field[i][j]) {
+                        path[i][j] = j;
+                    } else if (currentYield[i][j] == currentYield[i - 1][j - 1] + field[i][j]) {
+                        path[i][j] = j - 1;
+                    }
+                } // When diagonal left is not possible 
+                else if (j < currentYield[0].length - 1) {
+                    currentYield[i][j] = max(currentYield[i - 1][j], currentYield[i - 1][j + 1]) + currentYield[i][j];
+                    if (currentYield[i][j] == currentYield[i - 1][j] + field[i][j]) {
+                        path[i][j] = j;
+                    } else if (currentYield[i][j] == currentYield[i - 1][j + 1] + field[i][j]) {
+                        path[i][j] = j + 1;
+                    }
                 }
-                if (y[i][j] >= truffle) {
-                    truffle = y[i][j];
-                    temp[i] = j;
+                if (currentYield[i][j] >= maxTruffles) {
+                    maxTruffles = currentYield[i][j];
+                    lastRowMaxIndex = j;
                 }
-                truffle = max(y[i][j], truffle);
+                maxTruffles = max(currentYield[i][j], maxTruffles);
             }
-
-            //System.out.println("[" + (i + 1) + "," + (k + 1) + "] - " + field[i-1][s]);
-            //System.out.println(res);
         }
-        int var = temp.length;
-        int crap = temp[var - 1];
-        int holder = 0;
+        //int var = temp.length;
+        //int crap = temp[var - 1];
+        int pathOrigin = 0;
+        Deque<TruffleInfo> stack = new ArrayDeque<>();
 
-        int k = 0;
-        for (int i = y.length -1; i >= 0; i--) {
-            if (i == y.length -1) {
-                System.out.println("[" + (i + 1) + "," + (crap + 1) + "] - ");
+        for (int i = currentYield.length - 1; i >= 0; i--) {
+            TruffleInfo harvest = new TruffleInfo();
+            if (i == currentYield.length - 1) {
+                //harvest.row = i;
+                harvest.column = lastRowMaxIndex;
+                harvest.truffles = field[i][lastRowMaxIndex];
+                //stack.push(harvest);
             } else {
-                holder = path[i + 1][crap];
-                System.out.println("[" + (i + 1) + "," + (holder + 1) + "] - ");
-                crap = holder;
+                pathOrigin = path[i + 1][lastRowMaxIndex];
+                //harvest.row = i;
+                harvest.column = pathOrigin;
+                harvest.truffles = field[i][pathOrigin];
+                lastRowMaxIndex = pathOrigin;
+                //stack.push(harvest);
             }
+            harvest.row = i;
+            stack.push(harvest);
         }
 
-        //System.out.println("[1," + (temp[0]+1) + "] - ");
-        /*for (int i = temp.length - 1; i > 0; i--) {
-            holder = path[i][crap];
-            System.out.println("[" + (i + 1) + "," + (holder) + "] - ");
-            crap = path[i - 1][holder];
-        }*/
+        TruffleInfo t = new TruffleInfo();
+        while (!stack.isEmpty()) {
+            t = stack.pop();
+            System.out.println("[" + (t.row + 1) + "," + (t.column + 1) + "]-" + t.truffles);
+        }
+
         System.out.print("\n");
         System.out.print("\n");
-        for (int i = 0; i < y.length; i++) {
-            for (int j = 0; j < y[i].length; j++) {
-                System.out.print("\t" + y[i][j]);
+        for (int i = 0; i < currentYield.length; i++) {
+            for (int j = 0; j < currentYield[i].length; j++) {
+                System.out.print("\t" + currentYield[i][j]);
             }
             System.out.print("\n");
         }
@@ -157,8 +197,7 @@ public class TrufflePath {
             }
             System.out.print("\n");
         }
-
-        return truffle;
+        return maxTruffles;
     }
 
 }
